1729546a69c117f3055baf9f4d679545f9896c5d
Karl Fabe <karl@aloware.com>
2022-06-26 15:05:59 +0800

    Partial update

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,66 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <!-- <button @click="goToAbout">About 2</button>
+    <div>{{ age }}</div>
+    <input v-model="age" type="text"> -->
+    <!-- <ul>
+      <li v-for="(item, key) in fruits" :key="key">
+        The price for {{ item.name }} is {{ item.price }} == 
+        {{ `The price for ${item.name} is ${item.price}` }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="(item, key) in veggies" :key="key">
+        The price for {{ item.name }} is {{ item.price }} == 
+        {{ `The price for ${item.name} is ${item.price}` }}
+      </li>
+    </ul> -->
   </div>
   <router-view/>
 </template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const ctr = ref(0)
+    const veggies = ref([
+      { name: 'repolyo', price: 212 },
+      { name: 'pipino', price: 323 },
+      { name: 'carrots', price: 11 }
+    ])
+    
+    function test () {
+      console.log('Heyyyyyyyyyy')
+    }
+
+    return {
+      ctr,
+      veggies,
+      test
+    }
+  },
+  data () {
+    return {
+      age: 0,
+      fruits: [
+        { name: 'apple', price: 100 },
+        { name: 'orange', price: 200 },
+        { name: 'banana', price: 159 }
+      ]
+    }
+  },
+  methods: {
+    goToAbout () {
+      this.$router.push({ name: 'About' })
+    }
+  }
+}
+</script>
+
 
 <style>
 #app {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li v-for="(person, i) in people" :key="i">
+        {{ person }}
+      </li>
+    </ul>
+    <input v-model="name" type="text">
+    <button @click="addPerson">Add</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'About',
+    data () {
+      return {
+        people: ['Arnold'],
+        name: ''
+      }
+    },
+    methods: {
+      addPerson () {
+        console.log('Add a person')
+        this.people.push(this.name)
+        this.name = ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
